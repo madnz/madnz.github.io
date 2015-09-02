987f411e38695e68dc55266d6538c6b9e16d7129
Adrian Parker <adrian_parker@bnz.co.nz>
2015-09-02 15:50:59 +1200

    Update from ./MakeADifference

--- index.js
+++ index.js
@@ -2,11 +2,8 @@
 
 window.onload = function () {
 	document.getElementById('applyForm').onsubmit = function(e) {
-		if (e.preventDefault) {
-			e.preventDefault();
-		}
-
-		var applyForm = document.getElementById('applyForm'),
+		var firebase = new Firebase('https://scorching-fire-2466.firebaseIO.com/'),
+			applyForm = document.getElementById('applyForm'),
 			applicantName = applyForm.elements['name'].value,
 			applicantEmail = applyForm.elements['email'].value,
 			applicantSkills = applyForm.elements['skills'].value,
@@ -14,9 +11,16 @@ window.onload = function () {
 				"name": applicantName,
 				"email": applicantEmail,
 				"skills": applicantSkills
-			};
+			},
+			applicationRef;
+		if (e.preventDefault) {
+			e.preventDefault();
+		}
 
-		alert('form submitted with '+JSON.stringify(application));
+		// saves this volunteer application as a new unique entry in the volunteers list on Firebase
+		applicationRef = firebase.child('volunteers').push(application);
+
+		alert('Application saved with key: '+applicationRef.key());
 
 		// prevent form default behaviour
 		return false;
