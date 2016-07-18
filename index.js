8b30a4983b4c2d714dc0b6d5a4673a0d7d5f3832
Adrian Parker <adrian@iceknife.com>
2016-07-18 19:36:13 +1200

    Update from ./MakeADifference

--- index.js
+++ index.js
@@ -1,54 +1,78 @@
 "use strict";
 
+var firebase = new Firebase('https://scorching-fire-2466.firebaseIO.com/');
+
+
+function appendVacancy(text) {
+    var li = document.createElement('li');
+    li.innerHTML = text;;
+    document.getElementById('vacancies').appendChild(li);
+}
 
 window.onload = function () {
-    document.getElementById('applyForm').onsubmit = function (e) {
-        var firebase = new Firebase('https://scorching-fire-2466.firebaseIO.com/'),
-            applyForm = document.getElementById('applyForm'),
-            applicantName = applyForm.elements['name'].value,
-            applicantEmail = applyForm.elements['email'].value,
-            applicantSkills = applyForm.elements['skills'].value,
-            application = {
-                "name": applicantName,
-                "email": applicantEmail,
-                "skills": applicantSkills
-            },
-            volunteersRef,
-            newVolunteer,
-            newVolunteerKey,
-            formInError = false;
-        if (e.preventDefault) {
-            e.preventDefault();
+    document.getElementById('applyForm').onsubmit = apply;
+    var projectsRef = firebase.child('projects');
+    projectsRef.on("value", function (snapshot) {
+        document.getElementById('vacancies').innerHTML = '';
+        if (snapshot.hasChildren()) {
+            snapshot.forEach(generateVacancyLI);
+        } else {
+            appendVacancy('<h4>Sorry no vacancies are available</h4>');
         }
-        // validate inputs. Browser may do this for us, but it might not.
-        if (application.name === null || application.name === undefined || application.name.length === 0) {
-            formInError = setFormFieldError('name-error', 'Please enter your name');
-        }
-        if (application.skills === null || application.skills === undefined || application.skills.length === 0) {
-            formInError = setFormFieldError('skills-error', 'Please enter your skills');
-        }
-        if (application.email === null || application.email === undefined || application.email.length === 0) {
-            formInError = setFormFieldError('email-error', 'Please enter your email');
-        } else if (!validateEmail(application.email)) {
-            formInError = setFormFieldError('email-error', 'Please enter a valid email');
-        }
-        if (!formInError) {
-            // saves this volunteer application as a new unique entry in the volunteers list on Firebase
-            volunteersRef = firebase.child('volunteers');
-            newVolunteer = volunteersRef.push();
-            newVolunteer.set(application, function (error) {
-                if (error) {
-                    console.log("New volunteer data could not be saved: " + error);
-                    //TODO notify user of form submit failure
-                } else {
-                    newVolunteerKey = newVolunteer.key();
-                    window.location = 'thank-you.html?key=' + encodeURI(newVolunteerKey) + '&name=' + encodeURI(application.name);
-                }
-            });
-        }
-        // prevent form default behaviour
-        return false;
+    });
+}
+
+function generateVacancyLI(vacancy) {
+    var vacancyObj = vacancy.val(),
+        text = '<div><span>' + vacancyObj.vacancyDates + '</span> in <span>' + vacancyObj.vacancyLocations + '</span><br><span>' + vacancyObj.vacancyName + '</span><br><span>' + vacancyObj.organisationName + '</span> need help with <span>' + vacancyObj.vacancySkills + '</span><br>Details | Apply | Share</div>';
+    appendVacancy(text);
+}
+
+function apply(e) {
+    var applyForm = document.getElementById('applyForm'),
+        applicantName = applyForm.elements['name'].value,
+        applicantEmail = applyForm.elements['email'].value,
+        applicantSkills = applyForm.elements['skills'].value,
+        application = {
+            "name": applicantName,
+            "email": applicantEmail,
+            "skills": applicantSkills
+        },
+        volunteersRef,
+        newVolunteer,
+        newVolunteerKey,
+        formInError = false;
+    if (e.preventDefault) {
+        e.preventDefault();
     }
+    // validate inputs. Browser may do this for us, but it might not.
+    if (application.name === null || application.name === undefined || application.name.length === 0) {
+        formInError = setFormFieldError('name-error', 'Please enter your name');
+    }
+    if (application.skills === null || application.skills === undefined || application.skills.length === 0) {
+        formInError = setFormFieldError('skills-error', 'Please enter your skills');
+    }
+    if (application.email === null || application.email === undefined || application.email.length === 0) {
+        formInError = setFormFieldError('email-error', 'Please enter your email');
+    } else if (!validateEmail(application.email)) {
+        formInError = setFormFieldError('email-error', 'Please enter a valid email');
+    }
+    if (!formInError) {
+        // saves this volunteer application as a new unique entry in the volunteers list on Firebase
+        volunteersRef = firebase.child('volunteers');
+        newVolunteer = volunteersRef.push();
+        newVolunteer.set(application, function (error) {
+            if (error) {
+                console.log("New volunteer data could not be saved: " + error);
+                //TODO notify user of form submit failure
+            } else {
+                newVolunteerKey = newVolunteer.key();
+                window.location = 'thank-you.html?key=' + encodeURI(newVolunteerKey) + '&name=' + encodeURI(application.name);
+            }
+        });
+    }
+    // prevent form default behaviour
+    return false;
 }
 
 function setFormFieldError(fieldId, error) {
--- testLogin.js
+++ testLogin.js
@@ -1,32 +1,132 @@
 "use strict";
 
+var userUID, userName, userRole, volunteerTable, projectTable,
+    firebase = new Firebase('https://scorching-fire-2466.firebaseIO.com/');
+
 window.onload = function () {
-    document.getElementById('loginButton').onclick = function (e) {
-        var firebase = new Firebase('https://scorching-fire-2466.firebaseIO.com/');
-        firebase.authWithOAuthPopup("google", function(error, authData) {
-            if (error) {
-                console.log("Login Failed!", error);
-            } else {
-                console.log("Authenticated successfully with payload:", authData);
-                document.getElementById('welcomeName').innerHTML = authData.google.cachedUserProfile.given_name;
-                // update users table
-                var usersRef = firebase.child('users');
-                usersRef.child(authData.auth.uid).update({
-                    provider: authData.auth.provider,
-                    name: authData[authData.auth.provider].displayName,
-                    lastLogin: new Date()
-                }, function (error) {
-                    if (error) {
-                        console.log("New user data could not be saved: " + error);
-                    } else {
-                        // read the user list and display it. Security rules mean this should only show the logged in user.
-                        usersRef.once("value", function(data) {
-                            console.log(data.val());
-                        });
-                    }
-                });
-            }
-        });
-    };
+    document.getElementById('loginButton').onclick = login;
+    document.getElementById('volunteersAdminButton').onclick = volunteers;
+    document.getElementById('projectAdminButton').onclick = projects;
 }
+
+function showInWorkArea(html) {
+    document.getElementById('workArea').innerHTML = html;
+}
+
+function showWorkAreaLoading() {
+    showInWorkArea('<h4>loading...</h4>');
+}
+
+function volunteers() {
+    showWorkAreaLoading();
+    // get list of volunteers
+    var volunteersRef = firebase.child('volunteers');
+    volunteersRef.on("value", function (snapshot) {
+        if (snapshot.hasChildren()) {
+            volunteerTable = '<table id="volunteerTable"><thead><tr><th>Name</th><th>Email</th><th>Skills</th></tr></thead><tbody>';
+            snapshot.forEach(generateVolunteerHTML);
+            volunteerTable += '</tbody></table>';
+            // add link for exporting to csv
+            volunteerTable += '<br/><a download="volunteer.csv" href="#" onclick="return ExcellentExport.csv(this, \'volunteerTable\');">Export to CSV</a>';
+            showInWorkArea(volunteerTable);
+        } else {
+            showInWorkArea('<h4>No volunteers available</h4>');
+        }
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+}
+
+function generateVolunteerHTML(volunteer) {
+    var volunteerObj = volunteer.val();
+    volunteerTable += '<tr><td>' + volunteerObj.name + '</td><td>' + volunteerObj.email + '</td><td>' + volunteerObj.skills + '</td></tr>';
+}
+
+function generateProjectHTML(project) {
+    var projectObj = project.val();
+    projectTable += '<tr><td>' + projectObj.organisationName + '</td><td>' + projectObj.vacancyName + '</td></tr>';
+}
+
+function projects() {
+    console.log('projects');
+    projectTable = '';
+    showWorkAreaLoading();
+    // get list of projects
+    var projectsRef = firebase.child('projects');
+    projectsRef.on("value", function (snapshot) {
+        if (snapshot.hasChildren()) {
+            projectTable = '<table id="projectTable"><thead><tr><th>Organisation</th><th>Vacancy</th></tr></thead><tbody>';
+            snapshot.forEach(generateProjectHTML);
+            projectTable += '</tbody></table>';
+            // add link for exporting to csv
+            projectTable += '<br/><a download="project.csv" href="#" onclick="return ExcellentExport.csv(this, \'projectTable\');">Export to CSV</a>';
+        } else {
+            projectTable = '<h4>No projects available</h4>';
+        }
+        // add Add New link
+        projectTable += '<br/><a href="#" id="projectAddButton" onclick="projectAdd();">Add Project</a>';
+        showInWorkArea(projectTable);
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+}
+
+function createProject() {
+    console.log('create project');
+    document.getElementById('projectForm').style.display = 'none';
+    showWorkAreaLoading();
+    var newProject = {
+        "organisationName": document.getElementById('organisationName').value,
+        "organisationType": document.getElementById('organisationType').value,
+        "vacancyName": document.getElementById('vacancyName').value,
+        "vacancyDates": document.getElementById('vacancyDates').value,
+        "vacancyLocations": document.getElementById('vacancyLocations').value,
+        "vacancySkills": document.getElementById('vacancySkills').value,
+        "vacancyDescription": document.getElementById('vacancyDescription').value
+    };
+    var projectsRef = firebase.child('projects');
+    var newProjectRef = projectsRef.push(newProject);
+    console.log("Saved project with new key: "+newProjectRef.key());
+    projects();
+}
+
+function projectAdd() {
+    console.log('add project');
+    document.getElementById('projectForm').style.display = 'block';
+}
+
+function login() {
+    console.log('login');
+    firebase.authWithOAuthPopup("google", function (error, authData) {
+        if (error) {
+            console.log("Login Failed!", error);
+        } else {
+            document.getElementById('loginButton').style.display = 'none';
+            userUID = authData.auth.uid;
+            userName = authData.google.cachedUserProfile.given_name;
+            document.getElementById('welcomeName').innerHTML = userName;
+            // update users table
+            var usersRef = firebase.child('users');
+            usersRef.child(userUID).update({
+                provider: authData.auth.provider,
+                name: authData[authData.auth.provider].displayName,
+                lastLogin: new Date()
+            }, function (error) {
+                if (error) {
+                    console.log("New user data could not be saved: " + error);
+                } else {
+                    // read the user list and display it. Security rules mean this should only show the logged in user.
+                    usersRef.once("value", function (data) {
+                        userRole = data.val()[userUID].role;
+                        document.getElementById('role').innerHTML = userRole || 'Unknown';
+                        document.getElementById('lastLogin').innerHTML = data.val()[userUID].lastLogin;
+                        document.getElementById('loginInfo').style.display = 'block';
+                        document.getElementById('volunteersAdminButton').style.visibility = userRole === 'admin' ? 'visible' : 'hidden';
+                        document.getElementById('projectAdminButton').style.visibility = userRole === 'admin' ? 'visible' : 'hidden';
+                    });
+                }
+            });
+        }
+    });
+};
 
