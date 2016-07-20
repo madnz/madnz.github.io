83abd841f9c385fd1f348881489e1e29b7275b33
Adrian Parker <adrian@iceknife.com>
2016-07-20 23:16:15 +1200

    Update from ./MakeADifference

--- deployToLocalhost.sh
+++ deployToLocalhost.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'Deploy to localhost begun'
 rsync -av --exclude='node_modules' --exclude='.git' --exclude='.idea' --exclude='README.md' --exclude='deployToProd.sh' --exclude='ignore' ./ ~/Sites/madnz/
-open -a Safari http://localhost/~adrian/madnz/volunteering-opportunities.html
+open -a Safari http://localhost/~adrian/madnz
 echo 'Deploy to localhost completed'
